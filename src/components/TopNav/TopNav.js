eda6acfee67523c11a2d3b7e39ad80b313c2facf
amynguyendesign <110131305+amynguyendesign@users.noreply.github.com>
2023-07-20 19:51:00 +0000

    some code for the day

---
+++ src/components/TopNav/TopNav.js
@@ -0,0 +1,15 @@
+import './TopNav.css';
+
+function TopNav() {
+  return (
+    <header className="container">
+        <ul>
+            <li> home </li>
+            <li> work </li>
+            <li> play </li>
+        </ul>
+    </header>
+  );
+}
+
+export default TopNav;
