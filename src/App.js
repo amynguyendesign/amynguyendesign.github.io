a447c711d9614ab5920cb3fa2ad45c4291251af9
amynguyendesign <110131305+amynguyendesign@users.noreply.github.com>
2024-08-11 10:44:38 +0000

    woohoo new layout

--- src/App.js
+++ src/App.js
@@ -7,12 +7,16 @@ import './global.css';
 function HomePage() {
   return (
     <div className="corner-brackets">
-    {/* Your content here */}
-    <div className="content">
-      <div className='headline1'>coding + designing</div>
-      <div className='headline2'>in San Francisco or at a decaf-serving coffee shop near you.</div>
+      <div className="content">
+        <div className='headline1'>coding + designing</div>
+        <div className='headline2'>in San Francisco or at a decaf-serving coffee shop near you.</div>
+        <ul className='bodyText'>
+          <li>I made this website and a few other things   </li>    
+          Also working on some other fun things
+          <li> Startup founders: book a UX walkthrough/ product feedback session here </li>
+        </ul>
+      </div>
     </div>
-  </div>
   );
 }
 
