f92d2f23020219868cd413c6708ae4d951c787da
amynguyendesign <110131305+amynguyendesign@users.noreply.github.com>
2024-10-25 13:14:27 +0000

    responsive footer

--- src/App.js
+++ src/App.js
@@ -8,16 +8,14 @@ import Footer from './components/Footer/Footer';
 
 function HomePage() {
   return (
-    // <div className="corner-brackets">
       <div className="homeContainer">
         <div className='headline1'>Pushing pixels, writing code</div>
         <div className='headline2'>and probably convincing you to increase that <span className='inlineCode'> border-radius</span> just a tiiiny bit more</div>
         <ul className='bodyText'>
-        <li> Hi! It's Amy! I've spent the past 3 years building education tools as a full-time fullstack engineer.  </li>    
+        <li> Hi! I'm Amy! I've spent the past 3 years building education tools as a full-time fullstack engineer.  </li>    
         <li> Prior to that, I was at Stanford studying Computer Science and avoiding caterpillars (seriously, they were everywhere!) </li>
         </ul>
       </div>
-    // </div>
   );
 }
 
