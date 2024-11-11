c2ad56b1119605c3818c8b390222fe54387accf0
amynguyendesign <110131305+amynguyendesign@users.noreply.github.com>
2024-11-11 15:17:23 +0000

    bio fix

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function HomePage() {
   return (
       <div className="homeContainer">
         <div className='headline1'>Pushing pixels, writing code</div>
-        <div className='headline2'>and probably convincing you to increase that <span className='inlineCode'> border-radius</span> just a tiiiny bit more ♡</div>
+        <div className='headline2'>and probably convincing you to increase that <span className='inlineCode'> border-radius</span> just a tiny bit more.</div>
         <ul className='bodyText'>
         <li> Hi! I'm Amy! I've spent the past 3 years building education tools as a full-time fullstack engineer.  </li>    
         <li> Prior to that, I was at Stanford studying Computer Science and avoiding caterpillars. </li>
