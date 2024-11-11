f778114efffe85c850db4f4a4198942966421f87
amynguyendesign <110131305+amynguyendesign@users.noreply.github.com>
2024-11-11 15:49:10 +0000

    text styles

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function HomePage() {
       <div className="homeContainer">
         <div className='headline1'>Pushing pixels, writing code</div>
         <div className='headline2'>and probably convincing you to increase that <span className='inlineCode'> border-radius</span> just a tiny bit more.</div>
-        <ul className='bodyText'>
+        <ul>
         <li> Hi! I'm Amy! I've spent the past 3 years building education tools as a full-time fullstack engineer.  </li>    
         <li> Prior to that, I was at Stanford studying Computer Science and avoiding caterpillars. </li>
         </ul>
