03f6976df9580380400fb53f44263071acf90162
amynguyendesign <110131305+amynguyendesign@users.noreply.github.com>
2025-03-31 09:24:20 +0000

    ios cant take a comic sans joke smh

--- src/components/ContactPage/ContactPage.jsx
+++ src/components/ContactPage/ContactPage.jsx
@@ -25,8 +25,8 @@ function ContactPage() {
           LinkedIn
         </a>
         .<br />
-        If you want to nerd out about books, design, tech — or debate whether{" "}
-        <span className="comicSans">Comic Sans</span> is ever acceptable,{" "}
+        If you want to nerd out about books, design, tech — or debate whether
+        Comic Sans is ever acceptable,{" "}
         <a
           className="linkStyle"
           href="mailto:&#97;&#110;&#103;&#117;&#121;&#101;&#110;&#54;&#56;&#48;&#57;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;"
