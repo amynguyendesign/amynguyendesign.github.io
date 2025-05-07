eea2ee1f1036117d8060239f2a93cd679fc7c559
amynguyendesign <110131305+amynguyendesign@users.noreply.github.com>
2025-05-07 07:02:59 +0000

    new birdy painting

--- src/components/ArtPage/ArtPage.jsx
+++ src/components/ArtPage/ArtPage.jsx
@@ -5,12 +5,13 @@ import painting2 from "../../assets/onions.JPG";
 import painting3 from "../../assets/sf.jpg";
 import painting4 from "../../assets/coffee.jpg";
 import painting5 from "../../assets/spatula.JPG";
+import painting6 from "../../assets/bird.jpg"
 
 function ArtPage() {
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: "auto" });
   }, []);
-  const paintings = [painting1, painting2, painting3, painting4, painting5];
+  const paintings = [painting1, painting2, painting3, painting4, painting5, painting6];
   const [lightboxImage, setLightboxImage] = useState(null);
 
   const openLightbox = (image) => setLightboxImage(image);
