27458c43719b1ae0614e4a3dc4536093b356c934
amynguyendesign <110131305+amynguyendesign@users.noreply.github.com>
2025-10-26 08:37:27 +0000

    change copy

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -22,18 +22,17 @@ function AboutPage() {
         </p>
 
         <p>
-          With no prior technical experience, I started from scratch: multivariable calculus, first-order logic, my first lines of code. I gained a new mental framework that rewired how I solve problems.
+          With no prior technical experience, I started from scratch: multivariable calculus, first-order logic, my first lines of code. I gained a new mental framework that rewired how I solve problems and build solutions.
         </p>
 
         <p>
-          Today, I work at the intersection of engineering and design. I care
-          about clarity, structure, and intention -- whether in a line of code,
-          a layout decision, or a spacing that’s off by 2px and now I can’t
-          unsee it. Drawn to patterns: visual, conceptual and structural, I have a gut feeling for how things align, when they work, and where they break. I care deeply about aesthetics, and about the quiet magic that comes from things crafted with intention.
+          Today, I work at the intersection of software engineering and product design. I take pride in my work and am dedicated to perfecting my craft. Drawn to patterns: visual, conceptual and structural, I have a gut feeling for how things align, when they work, and where they break.
         </p>
 
+        <p>I also care deeply about aesthetics, and about the quiet magic that comes from things crafted with intention.</p>
+
         <p>
-        I thrive at the intersection of disciplines. These days I think they call that being a poly-hyphenate. Ultimately though, I just want to understand as many jokes from as many fields as possible.
+        I thrive at the intersection of disciplines. A poly-hyphenate, if you will. Ultimately though, I just want to understand as many jokes from as many fields as possible •ᴗ•
         </p>
       </div>
     </div>
