01832d83c7660519b9a919eda9ccc138f9df5a7e
amynguyendesign <110131305+amynguyendesign@users.noreply.github.com>
2025-10-26 09:21:03 +0000

    fix contact page

--- src/components/ContactPage/ContactPage.jsx
+++ src/components/ContactPage/ContactPage.jsx
@@ -15,25 +15,17 @@ function ContactPage() {
         Get in touch<span className="accent">.</span>
       </div>
       <p className="contactIntro">
-        I’m on{" "}
-        <a
-          className="linkStyle"
-          href="https://www.linkedin.com/in/amynguyen"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LinkedIn
-        </a>
-        .<br />
-        If you want to nerd out about books, design, tech -- or debate whether
+                If you want to nerd out about books, design, tech -- or debate whether
         Comic Sans is ever acceptable,{" "}
         <a
           className="linkStyle"
-          href="mailto:&#97;&#110;&#103;&#117;&#121;&#101;&#110;&#54;&#56;&#48;&#57;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;"
+          href="https://www.linkedin.com/in/nguyen-amy"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          shoot me an email
+         DM me on LinkedIn
         </a>
-        .
+        .<br />
       </p>
     </div>
   );
